06b461af73d4bb38223999b37d736e9824ee3b1e
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2024-09-16 02:04:46 -0300

    Steam User Item Class

---
+++ RiskOfCoins/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskOfCoins {
+	[Serializable]
+	internal class User {
+		public User(string id, string name) {
+			this.Id = id;
+			this.Name = name;
+		}
+
+		public string Id { get; private set; }
+		public string Name { get; private set; }
+
+		public override string ToString() {
+			return this.Name;
+		}
+	}
+}
